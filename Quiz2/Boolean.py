a580e98513e0e561ca8410b2a94464c69f7665a6
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-05 13:07:38 -0400

    more change

--- Quiz2/Boolean.py
+++ Quiz2/Boolean.py
@@ -13,5 +13,33 @@ print(10 < x < 15)
 x = 8
 print(x != 7)
 
+# more advanced questions
+a = "Hello, World!"
+print(a[1])
+
+
+b = "Hello, World!"
+print(b[2:5])
+
+
+a = "Hello, World!"
+print(len(a))
+
+
+a = "Hello, World!"
+print(a.lower())
+
+
+a = "Hello, World!"
+print(a.upper())
+
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+a = "Hello, World!"
+print(a.split(","))  # returns ['Hello', ' World!']
+
 
 # Boolean variables are either True or False.
