8b03bb0cceb74cbbfc789c3f1490d9e45b31e34b
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-05 10:56:18 -0400

    new change

--- Operation.py
+++ Operation.py
@@ -1,12 +1,12 @@
 """Operation	What it returns
 
-x + y	Sum of x and y
-x - y	Difference of x and y
--x	Changed sign of x
-+x	Identity of x
-x * y	Product of x and y
-x / y	Quotient of x and y
-x // y	Quotient from floor division of x and y
-x % y	Remainder of x / y
-x ** y	x to the y power
+   x + y	        Sum of x and y
+   x - y	        Difference of x and y
+   -x	            Changed sign of x
+   +x	            Identity of x
+   x * y	        Product of x and y
+   x / y	        Quotient of x and y
+   x // y	        Quotient from floor division of x and y
+   x % y	        Remainder of x / y
+   x ** y	        x to the y power
 """
