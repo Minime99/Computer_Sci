f649b643f882835ee4e62918486b9c9e8b138af2
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-07 22:19:00 -0400

    new try

--- Sandbox/testing.py
+++ Sandbox/testing.py
@@ -31,3 +31,28 @@ while (True):
     if card1 == card2:
         print("Same")
         break
+
+# basic caculator
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("enter 1,2,3,4")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Invalid input")
