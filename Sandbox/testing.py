9014558c4d324d84c324fae64fc88cf6f678d162
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-07 22:42:43 -0400

    quad

--- Sandbox/testing.py
+++ Sandbox/testing.py
@@ -56,3 +56,22 @@ elif choice == '4':
     print(num1, "/", num2, "=", divide(num1, num2))
 else:
     print("Invalid input")
+
+# Solve the quadratic equation ax**2 + bx + c = 0
+
+# import complex math module
+import cmath
+
+# To take coefficient input from the users
+a = float(input('Enter a: '))
+b = float(input('Enter b: '))
+c = float(input('Enter c: '))
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solutions are {0} and {1}'.format(sol1, sol2))
