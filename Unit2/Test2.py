09a44c259226780c8ce1b5b6a143839169281047
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-16 12:09:32 -0400

    more test qs

--- Unit2/Test2.py
+++ Unit2/Test2.py
@@ -35,30 +35,44 @@ tuples
 set
 dictionaries
 """
+# # REVIEW:
 
+# 1.
 x = str(12)
 y = str(13)
 print(x + y)
-
+# 2.
 x = int(input("first number"))
 y = int(input("other number"))
 print(x+y)
-
+# 3.
 x = input("lower letter")
 print(x.upper())
-
+# 4.
 x = ["apples", "bananas", "carrots", "gun"]
 print(x)
 x.remove("carrots")
 print(x)
-
+# 5.
 x = {"apples": 8, "bananas": 10}
 print(x)
 x.update({"guns": 4})
 print(x)
-
+# 6.
 x = input("8 letters pls")
 if len(x) < 8:
     print("nope")
 elif len(x) == 8:
     print("thx")
+# 7.
+x = set([1, 2, 3, 4])
+print(x)
+print(len(x))
+# 8.
+x = input("10 letters pls")
+print(x)
+print(x[3:6])
+# 9.
+x = input("10 letters pls")
+print(x)
+print(x[5:10])
