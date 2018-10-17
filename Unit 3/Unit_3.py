7b89a382b91c303d37b3c44ffb06ea889fd50c90
Noah <nrasiuk@torontoprepschool.com>
2018-10-17 14:40:51 -0400

    First of Unit 3

--- Unit 3/Unit_3.py
+++ Unit 3/Unit_3.py
@@ -17,3 +17,21 @@ fruits = ["apple", "banana", "cherry"]
 for x in adj:
   for y in fruits:
     print(x, y)
+
+for x in range(1,11):
+    for y in range(1,11):
+        print(x, 'times', y, 'is', x*y)
+
+total = 0
+for x in range(1000):
+    if x%3 == 0:
+        total = total + x
+    elif x%5 == 0:
+        total = total + x
+        print(total)
+print(total)
+
+numbes = []
+for x in range(1000):
+    if x%3 == 0 or x%5 == 0:
+        print(x)
