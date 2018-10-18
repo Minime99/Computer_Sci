5dd2fe75f98e40cec8c5589d83e8d2d19abc6127
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-18 14:35:36 -0400

    more

--- Unit3/Random.py
+++ Unit3/Random.py
@@ -5,3 +5,21 @@ print(x)
 
 # # HACK:
 import random
+
+while(True):
+    dice1 = (random.randint(1, 6))
+    dice2 = (random.randint(1, 6))
+
+    print(dice1)
+    print(dice2)
+
+    if dice1 == 6:
+        if dice2 == 6:
+            print("done")
+            break
+# or
+import random
+x = random.randint(1, 6)
+while x != 6:
+    x = random.randint(1, 6)
+    print(x)
