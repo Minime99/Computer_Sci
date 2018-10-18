0304a25f2b7f21342d67614bf026c5f4d9c78897
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-18 14:45:50 -0400

    quick

--- Unit3/Random.py
+++ Unit3/Random.py
@@ -23,3 +23,13 @@ x = random.randint(1, 6)
 while x != 6:
     x = random.randint(1, 6)
     print(x)
+
+import random
+randnum = (random.randint(1, 48))
+while(True):
+
+    guess = int(input("what is the number (1-48)"))
+
+    if guess == randnum:
+        print("you got it!")
+        break
