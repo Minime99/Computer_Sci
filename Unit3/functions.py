f75b0ae494d5b3b85943ae7d6276744ad8c1c2ab
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-24 19:50:26 -0400

    ya calling

---
+++ Unit3/functions.py
@@ -0,0 +1,16 @@
+def my_function():
+    print("hello from inside the function")
+
+
+my_function()
+
+# return values
+
+
+def dounble_it(x):
+    return 2*x
+
+
+print(dounble_it(2))
+print(dounble_it(5))
+print(dounble_it(9))
