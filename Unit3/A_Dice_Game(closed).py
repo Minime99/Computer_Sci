6b6a7a3356bfb710a2fb8257bd4113ef692e98e4
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-10-30 09:38:19 -0400

    go in

---
+++ Unit3/A_Dice_Game(closed).py
@@ -0,0 +1,31 @@
+import random
+
+
+def roll():
+    dice = (random.randint(1, 6))
+    return dice
+
+
+x = input("do you want to roll? (y)(n)")
+if x == "y":
+    roll
+
+
+dice1 = roll()
+print(dice1)
+dice2 = roll()
+print(dice2)
+
+
+def dice_total(dice1, dice2):
+    total = dice1 + dice2
+
+
+if dice1 == 1:
+    dice1 = 0 + dice_total
+elif roll == 1:
+    dice2 = 0 + dice_total
+elif dice1 + dice2 == 2:
+    dice_total = 0
+else:
+    dice_total = dice1 + dice2
---
+++ Unit3/Objects_Classes.py
@@ -0,0 +1,56 @@
+class pizza:
+    crust = "regular"
+    sauce = "tomato"
+    cheese = "mozzarella"
+    toppings = ["pepperoni", "mushrooms", "green peppers"]
+
+    def order(self):
+        print("the pizza has been ordered")
+
+
+Noah = pizza()
+aaron = pizza()
+donal = pizza()
+christan = pizza()
+
+"""
+print(Noah.crust)
+print(Noah.sauce)
+print(Noah.cheese)
+print(toppings[1])
+"""
+donal.toppings.append("pineapple")
+aaron.crust = "stuffed"
+
+print(Noah.crust)
+print(aaron.crust)
+print(donal.toppings)
+donal.order()
+
+
+class pizza:
+    def __init__(self):
+        self.crust = "regular"
+        self.sauce = "tomato"
+        self.cheese = "mozzarella"
+        self.toppings = ["pepperoni", "mushrooms", "green peppers"]
+
+    def order(self):
+        print("the pizza has been ordered")
+
+
+noah = pizza()
+print(noah.crust)
+
+
+class Dog:
+    def __init__(self):
+        self.age = 0
+        self.breed = "unkown"
+        self.shots = ["rabies", "lepto", "cancer"]
+
+    def __str__(self):
+        return(self.breed+""+str(self.age))
+
+
+Betsey = Dog()
