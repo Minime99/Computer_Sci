5d82c8d8bb850e23fb01a6d89d24162466934f49
Noah <nrasiuk@torontoprepschool.com>
2018-11-15 14:22:55 -0500

    new

--- Unit4(b_noah)/Unit 4.py
+++ Unit4(b_noah)/Unit 4.py
@@ -1,1 +1,33 @@
-f = open('sampletext.txt')
+''' Day 1       November 14'''
+
+#To find a file and use it somewhere
+#Type 'ls' in terminal
+#Type the folder by typing 'cd' then folder
+#When you find the file, type    python3 'name of file'
+#Must be .py file
+#'r' means read, 'w' means write, 'a' means append a str to end of .txt file
+
+f = open('sampletext.txt', 'r')
+x = f.readlines()
+for line in x:
+    print('Line! :',line)
+    print(line.replace(',no','ABSOLUTELY NOT'))
+    print(line.split(' ')[1].split(':'))
+
+
+
+'''Day 2       November 15'''
+
+print('Script Starting')
+f = open('sampletext.txt', 'r+w+a')
+f.readlines()
+f.write('This line is added to the file')
+f.append(str(x))
+print('Script Ending')
+
+#to remove files from os permanently
+import os
+try:
+    os.remove('sampletext.txt')
+except OSError:
+    print('Can not find file')
