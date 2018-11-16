77adcefea3d0a48d96cdaba479a1c3db7375fad8
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-11-16 13:48:59 -0500

    work

--- Unit4/script(In_Class).py
+++ Unit4/script(In_Class).py
@@ -1,3 +1,3 @@
-word = input("what do you want to search")
+word = raw_input("what do you want to search")
 f = open("/Users/noahfedosoff_S/Documents/Projects/Unit4/In_Class_Book.txt").read()
 print(f.count(word))
