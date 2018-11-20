e336c54a58832fa8dc776c93db3ccb38520b4755
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-11-20 11:12:29 -0500

    ok

--- Unit4/script(In_Class).py
+++ Unit4/script(In_Class).py
@@ -1,3 +1,4 @@
-word = raw_input("what do you want to search")
+word = raw_input("what do you want to search")  # sets input
+# open the file and reads all of it
 f = open("/Users/noahfedosoff_S/Documents/Projects/Unit4/In_Class_Book.txt").read()
-print(f.count(word))
+print(f.count(word))  # based on what you input this will give you a count
