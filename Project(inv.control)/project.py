e97f3bf135f6114c53c2ce3b95dfddaa0070ee12
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-11-27 09:38:47 -0500

    k

--- Project(inv.control)/project.py
+++ Project(inv.control)/project.py
@@ -1,3 +1,4 @@
+
 import datetime
 inv = {"Common nails": 11, "Box nails": 5, "Drywall nails": 20,
        "Double headed nails": 2, "Roofing nails": 13}
@@ -10,21 +11,32 @@ def saveinv():
     x.close()
 
 
+saveinv()
+
+
 def editinv(e):
     x1 = open("/Users/noahfedosoff_S/Documents/Projects/Project(inv.control)/inv.txt", "a")
     x1.write(e)
+    x1.close()
 
 
 c = input("what are you going to add to the inv?")
 editinv(c)
 
-
-def readinv():
-    x2 = open("/Users/noahfedosoff_S/Documents/Projects/Project(inv.control)/inv.txt", "r")
-    print(x2.read())
+r = input("do you want to see the new inv? [y,n]")
+if r == "y":
+    v = open("/Users/noahfedosoff_S/Documents/Projects/Project(inv.control)/inv.txt", "r")
+    print(v.read())
+    v.close()
 
 
 doyouwanttime = input("do you want the time? [y,n]")
 if doyouwanttime == "y":
     x = datetime.datetime.now()
     print(x)
+
+n = input("can you see the inv ok? [y,n]")
+if n == "n":
+    p = open("/Users/noahfedosoff_S/Documents/Projects/Project(inv.control)/inv.txt", "r")
+    print(p.read().upper())
+    p.close()
