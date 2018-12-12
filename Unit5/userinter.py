45219db484e0d7380d8b6cea9bf8133718b7e67e
Noah Fedosoff <nfedosoff@torontoprepschool.com>
2018-12-12 13:48:04 -0500

    userint

---
+++ Unit5/userinter.py
@@ -0,0 +1,7 @@
+"""
+text boxes: fill-in-the blank
+Radio buttons: allow you to select ONE option
+checkboxes: can select as many as you want
+Drop-down menus: Reveal choices when clicked, allow you to select one
+Toggles: Always have one or the other slected (best for on/off)
+"""
